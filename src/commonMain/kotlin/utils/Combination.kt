34ea0bd91ad9d74dd7adc8e281ed17b701387f79
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-07-31 19:13:02 +0600

    isolate logging of KSLog'gers to avoid problems with parallel logs perform

--- src/commonMain/kotlin/utils/Combination.kt
+++ src/commonMain/kotlin/utils/Combination.kt
@@ -4,6 +4,12 @@ import dev.inmo.kslog.common.CallbackKSLog
 import dev.inmo.kslog.common.KSLog
 
 infix operator fun KSLog.plus(other: KSLog) = CallbackKSLog { l, t, m, e ->
-    this@plus.performLog(l, t, m, e)
-    other.performLog(l, t, m, e)
+    val resultOfFirst = runCatching {
+        this@plus.performLog(l, t, m, e)
+    }
+    val resultOfSecond = runCatching {
+        other.performLog(l, t, m, e)
+    }
+    resultOfFirst.onFailure { throw it }
+    resultOfSecond.onFailure { throw it }
 }
